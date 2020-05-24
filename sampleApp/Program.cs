6b5a19f923373b2c92f2a325c572ef1141929790
srinirokz <srinivas.tdg@gmail.com>
2020-05-24 19:53:47 +1000

    initial commit

---
+++ sampleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            Program p = new Program();
+
+            p.Add(a, b);
+
+        }
+
+        public int Add(int i ,int j)
+        {
+            return i + j;
+        }
+    }
+}
